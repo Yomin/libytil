1e8d57b25cb774d6c90f35560989713673c83b8a
Yomin Nimoy <yomin.nimoy@hivegate.net>
2019-08-12 21:09:12 +0200

    add sys/proc, set/append proc title

---
+++ ytil/sys/proc.c
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2019 Martin Rödel a.k.a. Yomin Nimoy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "proc.h"
+#include <ytil/def.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+
+typedef struct proc_title
+{
+    char *title, *data;
+    size_t size, orig_len, ref;
+} proc_title_st;
+
+static struct proc_title  *proc_title;
+extern char              **environ;
+
+
+int proc_init_title(int argc, char *argv[])
+{
+    proc_title_st *title;
+    int envc, i;
+    
+    if(proc_title)
+        return proc_title->ref++, 0;
+    
+    if(!(title = calloc(1, sizeof(proc_title_st))))
+        return -1;
+    
+    for(envc=0; environ[envc]; envc++);
+    
+    if(!envc)
+        title->size = argv[argc-1] + strlen(argv[argc-1]) - argv[0] +1;
+    else
+        title->size = environ[envc-1] + strlen(environ[envc-1]) - argv[0] +1;
+    
+    if(title->size < 2)
+        return free(title), errno = ENOSPC, -1;
+    
+    if(!(title->data = malloc(title->size)))
+        return free(title), -1;
+    
+    memcpy(title->data, argv[0], title->size);
+    
+    for(i=1; i < argc; i++)
+        argv[i] = &title->data[argv[i] - argv[0]];
+    
+    for(i=0; i < envc; i++)
+        environ[i] = &title->data[environ[i] - argv[0]];
+    
+    title->title = argv[0];
+    title->orig_len = strlen(argv[0]);
+    proc_title = title;
+    
+    return 0;
+}
+
+void proc_free_title(void)
+{
+    if(proc_title)
+    {
+        if(proc_title->ref > 1)
+            proc_title->ref--;
+        else
+        {
+            free(proc_title->data);
+            free(proc_title);
+            proc_title = NULL;
+        }
+    }
+}
+
+int proc_set_title(const char *fmt, ...)
+{
+    va_list ap;
+    int rc;
+    
+    va_start(ap, fmt);
+    rc = proc_set_title_v(fmt, ap);
+    va_end(ap);
+    
+    return rc;
+}
+
+int proc_set_title_v(const char *fmt, va_list ap)
+{
+    return_err_if_fail(proc_title && fmt, EINVAL, -1);
+    
+    vsnprintf(proc_title->title, proc_title->size, fmt, ap);
+    proc_title->title[proc_title->size-1] = '\0';
+    
+    return 0;
+}
+
+int proc_append_title(const char *fmt, ...)
+{
+    va_list ap;
+    int rc;
+    
+    va_start(ap, fmt);
+    rc = proc_append_title_v(fmt, ap);
+    va_end(ap);
+    
+    return rc;
+}
+
+int proc_append_title_v(const char *fmt, va_list ap)
+{
+    char *title;
+    size_t size;
+    
+    return_err_if_fail(proc_title && fmt, EINVAL, -1);
+    
+    memcpy(proc_title->title, proc_title->data, proc_title->orig_len);
+    title = proc_title->title + proc_title->orig_len;
+    size = proc_title->size - proc_title->orig_len;
+    
+    vsnprintf(title, size, fmt, ap);
+    title[size-1] = '\0';
+    
+    return 0;
+}
---
+++ ytil/sys/proc.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019 Martin Rödel a.k.a. Yomin Nimoy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef __YTIL_SYS_PROC_H__
+#define __YTIL_SYS_PROC_H__
+
+#include <stdarg.h>
+
+
+// initialize proc title by scavenging argv and environ
+int proc_init_title(int argc, char *argv[]);
+// free proc title, bricks argv and environ
+void proc_free_title(void);
+
+// set process title
+int proc_set_title(const char *fmt, ...) __attribute__((format (gnu_printf, 1, 2)));
+// set process title, va_list version
+int proc_set_title_v(const char *fmt, va_list ap) __attribute__((format (gnu_printf, 1, 0)));
+
+// append to original process title
+int proc_append_title(const char *fmt, ...) __attribute__((format (gnu_printf, 1, 2)));
+// append to original process title, va_list version
+int proc_append_title_v(const char *fmt, va_list ap) __attribute__((format (gnu_printf, 1, 0)));
+
+#endif
