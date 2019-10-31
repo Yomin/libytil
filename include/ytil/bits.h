c94dfb178624e580c1e1650a6fdd28891dc1a234
Yomin Nimoy <yomin.nimoy@hivegate.net>
2019-10-31 13:08:11 +0100

    bits.h: fix BV and BM for variables > int; add generic FFS, CLZ, CTZ

--- include/ytil/bits.h
+++ include/ytil/bits.h
@@ -24,19 +24,21 @@
 #define __YTIL_BITS_H__
 
 #include <ytil/ext/endian.h>
+#include <assert.h>
 
 
 // bit value, lshift 1 'pos' times
 #define BV(pos) \
-    (1 << (pos))
+    (1LLU << (pos))
 
 // bit mask value, lshift 'mask' 'pos' times
 #define BMV(mask, pos) \
-    ((m) << (pos))
+    ((mask) << (pos))
 
 // bit mask of n ones
-#define BM(n) \
-    (BV(n)-1)
+#define BM(n) __extension__({ \
+    __auto_type _n = (n); \
+    _n >= sizeof(long long)*8 ? (unsigned long long)-1 : BV(_n)-1; })
 
 // bit get, get bit at 'pos'
 #define BG(value, pos) \
@@ -52,5 +54,45 @@
 #elif __BYTE_ORDER == __BIG_ENDIAN
     #define BA(value, n) (((unsigned char*)(value)) + (n))
 #endif
+
+// find first set
+#define FFS(mask) _Generic((mask) \
+    , char: __builtin_ffs(mask) \
+    , unsigned char: __builtin_ffs(mask) \
+    , short: __builtin_ffs(mask) \
+    , unsigned short: __builtin_ffs(mask) \
+    , int: __builtin_ffs(mask) \
+    , unsigned int: __builtin_ffs(mask) \
+    , long: __builtin_ffsl(mask) \
+    , unsigned long: __builtin_ffsl(mask) \
+    , long long: __builtin_ffsll(mask) \
+    , unsigned long long: __builtin_ffsll(mask))
+
+// count leading zeros
+#define CLZ(mask) _Generic((mask) \
+    , char: __builtin_clz(mask)-24 \
+    , unsigned char: __builtin_clz(mask)-24 \
+    , short: __builtin_clz(mask)-16 \
+    , unsigned short: __builtin_clz(mask)-16 \
+    , int: __builtin_clz(mask) \
+    , unsigned int: __builtin_clz(mask) \
+    , long: __builtin_clzl(mask) \
+    , unsigned long: __builtin_clzl(mask) \
+    , long long: __builtin_clzll(mask) \
+    , unsigned long long: __builtin_clzll(mask))
+
+// count trailing zeros
+#define CTZ(mask) _Generic((mask) \
+    , char: __builtin_ctz(mask) \
+    , unsigned char: __builtin_ctz(mask) \
+    , short: __builtin_ctz(mask) \
+    , unsigned short: __builtin_ctz(mask) \
+    , int: __builtin_ctz(mask) \
+    , unsigned int: __builtin_ctz(mask) \
+    , long: __builtin_ctzl(mask) \
+    , unsigned long: __builtin_ctzl(mask) \
+    , long long: __builtin_ctzll(mask) \
+    , unsigned long long: __builtin_ctzll(mask))
+
 
 #endif
